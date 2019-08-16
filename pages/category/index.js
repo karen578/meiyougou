95f4a61a9a3ba597ff71b085eb3f2ec0ba955a55
karen578 <Karen578@126.com>
2019-08-16 19:15:38 +0800

    分类页面的完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,29 @@
 // pages/category/index.js
+import request from '../../utils/request.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+data:{
+  categories:[],
+  current:0
+},
+onLoad(){
+  request({
+    url:"/categories"
+  }).then(res=>{
+    const {message}=res.data
+    // console.log(message)
+    this.setData({
+      categories:message
+    })
+  })
+},
+// 点击分类触发的事件
+  handleClick(event){
+    const { index } = event.currentTarget.dataset
+    
+    this.setData({
+      current:index
+    })
 
   }
+
 })
